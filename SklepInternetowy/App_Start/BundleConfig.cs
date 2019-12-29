fca4f6a6fe9a67851ed6e55eca60035be4722378
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2019-12-29 17:15:07 +0100

    Add filtering and autocomplete to courses list

---
+++ SklepInternetowy/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SklepInternetowy.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jqueryAndJqueryUi").Include(
+                                         "~/Scripts/jquery-3.4.1.min.js",
+                                         "~/Scripts/jquery-ui-1.12.1.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                                        "~/Content/themes/base/core.css",
+                                        "~/Content/themes/base/autocomplete.css",
+                                        "~/Content/themes/base/theme.css",
+                                        "~/Content/themes/base/menu.css"));
+        }
+    }
+}
--- SklepInternetowy/Controllers/KursyController.cs
+++ SklepInternetowy/Controllers/KursyController.cs
@@ -16,11 +16,19 @@ namespace SklepInternetowy.Controllers
             return View();
         }
 
-        // GET: Lista kursów - bestsellery i nowości
-        public ActionResult Lista(string nazwaKategorii)
+        // GET: Lista kursów - bestsellery, nowości, filtrowanie
+        public ActionResult Lista(string nazwaKategorii, string searchQuery = null)
         {
             var kategoria = db.Kategorie.Include("Kursy").Where(k => k.NazwaKategorii.ToUpper() == nazwaKategorii.ToUpper()).Single();
-            var kursy = kategoria.Kursy.ToList();
+            var kursy = kategoria.Kursy.Where(a => (searchQuery == null ||
+                                              a.TytulKursu.ToLower().Contains(searchQuery.ToLower()) ||
+                                              a.AutorKursu.ToLower().Contains(searchQuery.ToLower())) &&
+                                              !a.Ukryty);
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_KursyList", kursy);
+            }
+
             return View(kursy);
         }
 
@@ -38,6 +46,13 @@ namespace SklepInternetowy.Controllers
         {
             var kategorie = db.Kategorie.ToList();
             return PartialView("_KategorieMenu", kategorie);
+        }
+
+        public ActionResult KursyPodpowiedzi(string term)
+        {
+            var kursy = db.Kursy.Where(a => !a.Ukryty && a.TytulKursu.ToLower().Contains(term.ToLower()))
+                        .Take(5).Select(a => new { label = a.TytulKursu });
+            return Json(kursy, JsonRequestBehavior.AllowGet);
         }
     }
 }
