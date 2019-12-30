706a5bff2c10ac61277debb9a11b370ff227bad5
Kajetan Pawliszyn <kpawliszyn@future-processing.com>
2019-12-30 10:00:20 +0100

    Add basic basket view and action for adding to basket

--- SklepInternetowy/Controllers/KoszykController.cs
+++ SklepInternetowy/Controllers/KoszykController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SklepInternetowy.DAL;
+using SklepInternetowy.Infrastructure;
+using SklepInternetowy.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +11,35 @@ namespace SklepInternetowy.Controllers
 {
     public class KoszykController : Controller
     {
+        private KoszykManager koszykManager;
+        private ISessionManager sessionManager { get; set; }
+        private KursyContext db;
+
+        public KoszykController()
+        {
+            db = new KursyContext();
+            sessionManager = new SessionManager();
+            koszykManager = new KoszykManager(sessionManager, db);
+        }
+
         // GET: Koszyk
         public ActionResult Index()
         {
-            return View();
+            var pozycjeKoszyka = koszykManager.PobierzKoszyk();
+            var cenaCalkowita = koszykManager.PobierzWartoscKoszyka();
+            KoszykViewModel koszykVM = new KoszykViewModel()
+            {
+                PozycjeKoszyka = pozycjeKoszyka,
+                CenaCalkowita = cenaCalkowita
+            };
+            return View(koszykVM);
         }
 
         // Dodaj do koszyka
-        public ActionResult DodajDoKoszyka(string id)
+        public ActionResult DodajDoKoszyka(int id)
         {
-            return View();
+            koszykManager.DodajDoKoszyka(id);
+            return RedirectToAction("Index");
         }
     }
 }
